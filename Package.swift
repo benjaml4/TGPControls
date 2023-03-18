79bdc9fd20a65b94f559803216753c9f64bd96ce
benjaml4 <benjaml4@users.noreply.github.com>
2023-03-18 11:07:12 +0000

    Update supported platform  to iOS v16.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // import PackageDescription
 
 let package = Package(
     name: "TGPControls",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v16)
     ],
     products: [
         .library(
